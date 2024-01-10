035c957f1ecffcc2dfd84c60c742782a0418248d
Sewere <39487964+Sewere@users.noreply.github.com>
2024-01-10 14:46:28 +0200

    1.6 task done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,10 +14,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral +1)
+  }
+  const handleBad = () => {
+    setBad(bad +1)
+  }
+
   return (
     <div>
       <h1>Give feedback</h1>
+      <Button text="Good" handleClick={handleGood}/><Button text="Neutral" handleClick={handleNeutral}/><Button text="Bad" handleClick={handleBad}/>
       <h1>Statistics</h1>
+      <Display value={"Good: "+good}/><Display value={"Neutral: "+neutral}/><Display value={"Bad: "+bad}/>
     </div>
   )
 }
