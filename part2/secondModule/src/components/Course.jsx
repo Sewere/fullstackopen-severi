6bb9b869e0955880ba257f69a29c634092ce5755
Sewere <39487964+Sewere@users.noreply.github.com>
2024-01-18 13:07:50 +0200

    2.3 works

--- part2/secondModule/src/components/Course.jsx
+++ part2/secondModule/src/components/Course.jsx
@@ -1,12 +1,13 @@
 const Course = ({ course }) => {
   console.log('hey!', course)
-  //const { course } = props
+  const totalExercises = course.parts.reduce((sum, course) => sum + course.exercises, 0)
     return (
       <div>
         <h1>{course.name}</h1>
         <ul>
         {course.parts.map(note => <li key={note.id}>{note.name}  {note.exercises}</li>)}
         </ul>
+        <b>Total number of exercises: {totalExercises}</b>
       </div>
     )
   }
