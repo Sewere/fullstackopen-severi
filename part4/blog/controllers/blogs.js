78b6533f811d32229a9f5c8bb11f280721828f1a
Sewere <39487964+Sewere@users.noreply.github.com>
2024-02-15 13:55:03 +0200

    4.13 and 4.14 works

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -20,4 +20,36 @@ router.post('/api/blogs', async (request, response, next) => {
   }
 })
 
+router.delete('/api/blogs/:id', async (request, response, next) => {
+  try {
+    const { id } = request.params
+    const deletedBlog = await Blog.findByIdAndDelete(id)
+    if (!deletedBlog) {
+      return response.status(404).json({ error: 'Blog not found' })
+    }
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/api/blogs/:id', async (request, response, next) => {
+  try {
+    const { id } = request.params
+    const { title, author, url, likes } = request.body
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id,
+      { title, author, url, likes },
+      { new: true }
+    )
+    if (!updatedBlog) {
+      return response.status(404).json({ error: 'Blog not found' });
+    }
+    response.json(updatedBlog);
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
--- part4/blog/tests/blog.test.js
+++ part4/blog/tests/blog.test.js
@@ -45,8 +45,61 @@ test('the blog count rises when a new blog is added', async () => {
   .expect(201)
 
   const response = await api.get('/api/blogs')
-  
+
   expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+})
+
+describe('testing deletion', () => {
+
+  test('deleting a single blog', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+
+    const ids = blogsAtEnd.map(b => b.id)
+    expect(ids).not.toContain(blogToDelete.id)
+  })
+
+  test('deleting a blog with wrong id', async () => {
+    const nonExistingId = await helper.nonExistingId()
+    
+    await api
+      .delete(`/api/blogs/${nonExistingId}`)
+      .expect(404)
+  })
+})
+
+test('updating an existing blog', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToUpdate = blogsAtStart[0]
+
+  const updatedBlogData = {
+    title: 'test title2',
+    author: 'test author2',
+    url: 'http://test.com/2',
+    likes: 200
+  }
+
+  await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(updatedBlogData)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const updatedBlog = blogsAtEnd.find(b => b.id === blogToUpdate.id)
+
+  expect(updatedBlog).toBeDefined()
+  expect(updatedBlog.title).toBe(updatedBlogData.title)
+  expect(updatedBlog.author).toBe(updatedBlogData.author)
+  expect(updatedBlog.url).toBe(updatedBlogData.url)
+  expect(updatedBlog.likes).toBe(updatedBlogData.likes)
 })
 
 afterAll(() => {
